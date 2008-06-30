f6cba128e780163f68d6465f659334c4aeebc83c
alexxed <devnull@localhost>
2008-06-30 20:43:51 +0000

    - added the database configuration back - moved the directory and database check in prepend.inc.php

--- narro/includes/prepend.inc.php
+++ narro/includes/prepend.inc.php
@@ -21,6 +21,36 @@
         define('__PREPEND_INCLUDED__', 1);
 
         require(dirname(__FILE__) . '/configuration.inc.php');
+
+        if (!file_exists(__DOCROOT__ . __SUBDIRECTORY__ . '/data'))
+            die(sprintf('Please create a directory "data" in %s and give it write permissions for everyone (chmod 777)', __DOCROOT__ . __SUBDIRECTORY__));
+
+        foreach (array('cache', 'cache/i18n', 'cache/zend', 'dictionaries', 'import', 'tmp') as $strDirName) {
+            if (!file_exists(__DOCROOT__ . __SUBDIRECTORY__ . '/data/' . $strDirName)) {
+                if (!mkdir(__DOCROOT__ . __SUBDIRECTORY__ . '/data/' . $strDirName))
+                    die(sprintf('Could not create a directory. Please create the directory "%s" and give it write permissions for everyone (chmod 777)', __DOCROOT__ . __SUBDIRECTORY__ . '/data/' . $strDirName));
+                else
+                    chmod(__DOCROOT__ . __SUBDIRECTORY__ . '/data/' . $strDirName, 0777);
+            }
+        }
+
+        $arrConData = unserialize(DB_CONNECTION_1);
+
+        $link = mysql_connect($arrConData['server'].(($arrConData['port'])?':' . $arrConData['port']:''), $arrConData['username'], $arrConData['password']);
+        if (!$link) {
+            print(sprintf('Unable to connect to the dabase. Please check database settings in file "%s"', dirname(__FILE__) . '/db.inc.php') . '<br />');
+            print(sprintf('Error: "%s"', mysql_error()));
+            die();
+        }
+
+        if (!mysql_select_db($arrConData['database'], $link)) {
+            print(sprintf('Unable to connect to the dabase. Please check database settings in file "%s"', dirname(__FILE__) . '/db.inc.php') . '<br />');
+            print(sprintf('Error: "%s"', mysql_error()));
+            die();
+        }
+
+
+
         require(__QCODO_CORE__ . '/qcodo.inc.php');
         require_once(dirname(__FILE__) . '/application.inc.php');
     }
