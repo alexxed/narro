442d781486c8289c2cfffb006406976f42bb1fcd
alexxed <devnull@localhost>
2008-12-31 13:47:49 +0000

    Encoded the text passed as search parameter

--- narro/rss.php
+++ narro/rss.php
@@ -501,7 +501,7 @@
                                             __VIRTUAL_DIRECTORY__ .
                                             __SUBDIRECTORY__ .
                                             '/' .
-                                            NarroLink::ContextSuggest($objContext->ProjectId, $objContext->FileId, $objContext->ContextId, 1, 1, "%27" . $objTextComment->Text->TextValue . "%27") .
+                                            NarroLink::ContextSuggest($objContext->ProjectId, $objContext->FileId, $objContext->ContextId, 1, 1, urlencode($objTextComment->Text->TextValue)) .
                                             '#textcomments';
                             }
 
