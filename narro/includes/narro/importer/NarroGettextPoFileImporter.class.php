6983183fc5f576e11c48c8d6a555336c2225f4cb
alexxed <devnull@localhost>
2009-03-02 14:07:00 +0000

    Fixed: access keys were not detected in case of single words

--- narro/includes/narro/importer/NarroGettextPoFileImporter.class.php
+++ narro/includes/narro/importer/NarroGettextPoFileImporter.class.php
@@ -560,15 +560,24 @@
              * it's a access key if _ is found only once
              */
             if (strstr($strCleanText, '_') && !strstr(NarroString::Replace('_', '', $strCleanText, 1), '_')) {
-                $arrPossibleWords = explode(' ', $strCleanText);
-                foreach($arrPossibleWords as $strPossibleWord) {
-                    /**
-                     * if there's a _ and the word that contains it starts with a capital letter or is a number, + or -
-                     */
-                    if (strstr($strPossibleWord, '_') && preg_match('/^[A-Z0-9\+\-]/', str_replace('_', '', $strPossibleWord))) {
+                if (strstr($strCleanText, ' ')) {
+                    $arrPossibleWords = explode(' ', $strCleanText);
+                    foreach($arrPossibleWords as $strPossibleWord) {
+                        /**
+                         * if there's a _ and the word that contains it starts with a capital letter or is a number, + or -
+                         */
+                        if (strstr($strPossibleWord, '_') && preg_match('/^[A-Z0-9\+\-]/', str_replace('_', '', $strPossibleWord))) {
+                            preg_match('/_(\w)/', $strText, $arrMatches);
+                            return array(NarroString::Replace('_' . $arrMatches[1], $arrMatches[1], $strText), '_', $arrMatches[1]);
+                        }
+                    }
+                }
+                else {
+                    if (strstr($strCleanText, '_') && preg_match('/^[A-Z0-9\+\-]/', str_replace('_', '', $strCleanText))) {
                         preg_match('/_(\w)/', $strText, $arrMatches);
                         return array(NarroString::Replace('_' . $arrMatches[1], $arrMatches[1], $strText), '_', $arrMatches[1]);
                     }
+
                 }
             }
 
@@ -576,15 +585,23 @@
              * it's a access key if & is found only once
              */
             if (strstr($strCleanText, '&') && !strstr(NarroString::Replace('&', '', $strCleanText, 1), '&')) {
-                $arrPossibleWords = explode(' ', $strCleanText);
-                foreach($arrPossibleWords as $strPossibleWord) {
-                    /**
-                     * if there's a _ and the word that contains it starts with a capital letter or is a number, + or -
-                     */
-                    if (strstr($strPossibleWord, '&') && preg_match('/^[A-Z0-9\+\-]/', str_replace('&', '', $strPossibleWord))) {
+                if (strstr($strCleanText, ' ')) {
+                    $arrPossibleWords = explode(' ', $strCleanText);
+                    foreach($arrPossibleWords as $strPossibleWord) {
+                        /**
+                         * if there's a _ and the word that contains it starts with a capital letter or is a number, + or -
+                         */
+                        if (strstr($strPossibleWord, '&') && preg_match('/^[A-Z0-9\+\-]/', str_replace('&', '', $strPossibleWord))) {
+                            preg_match('/&(\w)/', $strText, $arrMatches);
+                            return array(NarroString::Replace('&' . $arrMatches[1], $arrMatches[1], $strText), '&', $arrMatches[1]);
+                        }
+                    }
+                } else {
+                    if (strstr($strCleanText, '&') && preg_match('/^[A-Z0-9\+\-]/', str_replace('&', '', $strCleanText))) {
                         preg_match('/&(\w)/', $strText, $arrMatches);
                         return array(NarroString::Replace('&' . $arrMatches[1], $arrMatches[1], $strText), '&', $arrMatches[1]);
                     }
+
                 }
             }
 
