1bf923a4ef0180c4a7ec60aa6c973e079f035c2b
Alexandru Szasz <alexxed@gmail.com>
2010-05-21 05:49:09 +0000

    If we're in production mode, log the errors and show a friendly message.

--- narro/includes/configuration/configuration.inc.php
+++ narro/includes/configuration/configuration.inc.php
@@ -157,14 +157,16 @@ if (!defined('SERVER_INSTANCE')) {
             define('ERROR_PAGE_PATH', __PHP_ASSETS__ . '/error_page.php');
 
             // Define the Filepath for any logged errors
-            define('ERROR_LOG_PATH', __INCLUDES__ . '/error_log');
+            define('ERROR_LOG_PATH', __TMP_PATH__ . '/error_log');
 
-            // To Log ALL errors that have occurred, set flag to true
-//			define('ERROR_LOG_FLAG', true);
+            if (SERVER_INSTANCE == 'prod') {
+                // To Log ALL errors that have occurred, set flag to true
+                define('ERROR_LOG_FLAG', true);
 
-            // To enable the display of "Friendly" error pages and messages, define them here (path MUST be relative from the DOCROOT)
-//			define('ERROR_FRIENDLY_PAGE_PATH', __PHP_ASSETS__ . '/friendly_error_page.php');
-//			define('ERROR_FRIENDLY_AJAX_MESSAGE', 'Oops!  An error has occurred.\r\n\r\nThe error was logged, and we will take a look into this right away.');
+                // To enable the display of "Friendly" error pages and messages, define them here (path MUST be relative from the DOCROOT)
+                define('ERROR_FRIENDLY_PAGE_PATH', __PHP_ASSETS__ . '/friendly_error_page.php');
+                define('ERROR_FRIENDLY_AJAX_MESSAGE', 'Oops!  An error has occurred.\r\n\r\nThe error was logged, and we will take a look into this right away.');
+            }
 
             break;
     }
