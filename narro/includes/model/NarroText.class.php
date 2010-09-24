e6f7f1244c496052919de77778aa5197335326cb
Alexandru Szasz <alexxed@gmail.com>
2010-09-24 19:58:45 +0000

    Removed file caching, it's slowing things down.

--- narro/includes/model/NarroText.class.php
+++ narro/includes/model/NarroText.class.php
@@ -44,28 +44,6 @@
             return sprintf('NarroText Object %s',  $this->intTextId);
         }
 
-        public static function Load($intTextId, $objOptionalClauses = null) {
-            $strCacheId = __CLASS__ . __FUNCTION__ . 't' . $intTextId . 'o' . md5(serialize($objOptionalClauses));
-            $mixResult = QApplication::$Cache->load($strCacheId);
-            if ($mixResult === false) {
-                $mixResult = parent::Load($intTextId, $objOptionalClauses);
-                if ($mixResult !== false)
-                    QApplication::$Cache->save($mixResult, $strCacheId, array('text'), QDateTimeSpan::SecondsPerHour);
-            }
-            return $mixResult;
-        }
-
-        public static function LoadByTextValueMd5($strTextValueMd5, $objOptionalClauses = null) {
-            $strCacheId = __CLASS__ . __FUNCTION__ . 't' . $strTextValueMd5 . 'o' . md5(serialize($objOptionalClauses));
-            $mixResult = QApplication::$Cache->load($strCacheId, $objOptionalClauses);
-            if ($mixResult === false) {
-                $mixResult = parent::LoadByTextValueMd5($strTextValueMd5);
-                if ($mixResult !== false)
-                    QApplication::$Cache->save($mixResult, $strCacheId, array('text'), QDateTimeSpan::SecondsPerHour);
-            }
-            return $mixResult;
-        }
-
         public function Save($blnForceInsert = false, $blnForceUpdate = false) {
             $this->intTextWordCount = NarroString::WordCount($this->strTextValue);
             $this->intTextCharCount = strlen($this->strTextValue);
@@ -75,114 +53,8 @@
             else
                 $this->dttModified = QDateTime::Now();
             parent::Save($blnForceInsert, $blnForceUpdate);
+
         }
 
-        // Override or Create New Load/Count methods
-        // (For obvious reasons, these methods are commented out...
-        // but feel free to use these as a starting point)
-/*
-        public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-            // This will return an array of NarroText objects
-            return NarroText::QueryArray(
-                QQ::AndCondition(
-                    QQ::Equal(QQN::NarroText()->Param1, $strParam1),
-                    QQ::GreaterThan(QQN::NarroText()->Param2, $intParam2)
-                ),
-                $objOptionalClauses
-            );
-        }
-
-        public static function LoadBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-            // This will return a single NarroText object
-            return NarroText::QuerySingle(
-                QQ::AndCondition(
-                    QQ::Equal(QQN::NarroText()->Param1, $strParam1),
-                    QQ::GreaterThan(QQN::NarroText()->Param2, $intParam2)
-                ),
-                $objOptionalClauses
-            );
-        }
-
-        public static function CountBySample($strParam1, $intParam2, $objOptionalClauses = null) {
-            // This will return a count of NarroText objects
-            return NarroText::QueryCount(
-                QQ::AndCondition(
-                    QQ::Equal(QQN::NarroText()->Param1, $strParam1),
-                    QQ::Equal(QQN::NarroText()->Param2, $intParam2)
-                ),
-                $objOptionalClauses
-            );
-        }
-
-        public static function LoadArrayBySample($strParam1, $intParam2, $objOptionalClauses) {
-            // Performing the load manually (instead of using Qcodo Query)
-
-            // Get the Database Object for this Class
-            $objDatabase = NarroText::GetDatabase();
-
-            // Properly Escape All Input Parameters using Database->SqlVariable()
-            $strParam1 = $objDatabase->SqlVariable($strParam1);
-            $intParam2 = $objDatabase->SqlVariable($intParam2);
-
-            // Setup the SQL Query
-            $strQuery = sprintf('
-                SELECT
-                    `narro_text`.*
-                FROM
-                    `narro_text` AS `narro_text`
-                WHERE
-                    param_1 = %s AND
-                    param_2 < %s',
-                $strParam1, $intParam2);
-
-            // Perform the Query and Instantiate the Result
-            $objDbResult = $objDatabase->Query($strQuery);
-            return NarroText::InstantiateDbResult($objDbResult);
-        }
-*/
-
-
-
-        // Override or Create New Properties and Variables
-        // For performance reasons, these variables and __set and __get override methods
-        // are commented out.  But if you wish to implement or override any
-        // of the data generated properties, please feel free to uncomment them.
-/*
-        protected $strSomeNewProperty;
-
-        public function __get($strName) {
-            switch ($strName) {
-                case 'SomeNewProperty': return $this->strSomeNewProperty;
-
-                default:
-                    try {
-                        return parent::__get($strName);
-                    } catch (QCallerException $objExc) {
-                        $objExc->IncrementOffset();
-                        throw $objExc;
-                    }
-            }
-        }
-
-        public function __set($strName, $mixValue) {
-            switch ($strName) {
-                case 'SomeNewProperty':
-                    try {
-                        return ($this->strSomeNewProperty = QType::Cast($mixValue, QType::String));
-                    } catch (QInvalidCastException $objExc) {
-                        $objExc->IncrementOffset();
-                        throw $objExc;
-                    }
-
-                default:
-                    try {
-                        return (parent::__set($strName, $mixValue));
-                    } catch (QCallerException $objExc) {
-                        $objExc->IncrementOffset();
-                        throw $objExc;
-                    }
-            }
-        }
-*/
     }
 ?>
