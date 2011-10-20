64bc13e4a57efe99192309cea185b5bf34d3949b
Alexandru Szasz <alexxed@gmail.com>
2011-10-20 09:23:54 +0300

    If an invalid form state is found, log the error and continue creating a new form state.

--- narro/includes/qcubed/includes/qcubed/_core/base_controls/QFormBase.class.php
+++ narro/includes/qcubed/includes/qcubed/_core/base_controls/QFormBase.class.php
@@ -169,8 +169,14 @@
                     // We might have a valid form state -- let's see by unserializing this object
                     $objClass = QForm::Unserialize($strPostDataState);
 
-                // If there is no QForm Class, then we have an Invalid Form State
-                if (!$objClass) throw new QInvalidFormStateException($strFormId);
+                try {
+                    // If there is no QForm Class, then we have an Invalid Form State
+                    if (!$objClass) throw new QInvalidFormStateException($strFormId);
+                }
+                catch (QInvalidFormStateException $objEx) {
+                    error_log(__CLASS__ . __FUNCTION__ . __LINE__ . ':' . $objEx->getMessage());
+                    // continue as if there was no form state
+                }
             }
 
             if ($objClass) {
