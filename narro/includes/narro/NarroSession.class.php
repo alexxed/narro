105311b988858a672ee723bae05d8c563e0838f2
Alexandru Szasz <alexxed@gmail.com>
2011-11-24 22:22:09 +0200

    The session is now up to 7 days.

--- narro/includes/narro/NarroSession.class.php
+++ narro/includes/narro/NarroSession.class.php
@@ -17,9 +17,10 @@
      */
     class NarroSession {
         public function __construct() {
-            session_set_cookie_params(86400, __VIRTUAL_DIRECTORY__ . __SUBDIRECTORY__, null, null, true);
+            // 7 days
+            session_set_cookie_params(604800, __VIRTUAL_DIRECTORY__ . __SUBDIRECTORY__, null, null, true);
             session_name('narro');
-            session_cache_expire(86400);
+            session_cache_expire(604800);
             $blnResult = session_start();
             if (!$blnResult)
                 throw new Exception('Failed to start a session');
