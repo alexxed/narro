06175377d0888b56b567ecebd355256bcfa090e0
Alexandru Szasz <alexxed@gmail.com>
2012-06-11 22:04:55 +0300

    Added a custom save path for session data.

--- narro/includes/narro/NarroSession.class.php
+++ narro/includes/narro/NarroSession.class.php
@@ -18,6 +18,7 @@
     class NarroSession {
         public function __construct() {
             // 7 days
+            session_save_path(__TMP_PATH__);
             session_set_cookie_params(604800, __VIRTUAL_DIRECTORY__ . __SUBDIRECTORY__, null, ($_SERVER["HTTPS"] != ''), true);
             session_name('narro');
             session_cache_expire(604800);
